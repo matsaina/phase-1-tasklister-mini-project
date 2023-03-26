41843e720c00d579627a25443868e1f511c17ab6
matsaina <matsaina@gmail.com>
2023-03-26 21:32:34 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    buildData(e.target.new_task.value);
+  });
+  form.reset();
 });
+
+function buildData(form) {
+  event.preventDefault();
+
+  let btn = document.createElement("button");
+
+  btn.addEventListener("click", (e) => {
+    e.target.parentNode.remove();
+  });
+
+  let tasks = document.getElementById("tasks");
+  let li = document.createElement("li");
+
+  li.textContent = form;
+  btn.textContent = "X";
+
+  li.appendChild(btn);
+  console.log(li);
+  tasks.appendChild(li);
+}
